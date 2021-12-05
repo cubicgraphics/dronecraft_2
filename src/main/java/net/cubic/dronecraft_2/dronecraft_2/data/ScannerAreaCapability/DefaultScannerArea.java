fb9f1060c5b737ba96a21740f06dbcb1a6bf5eb8
cubic <cubicgraphics98@gmail.com>
2021-12-05 05:28:46 +0000

    Should be committed though main account

--- src/main/java/net/cubic/dronecraft_2/dronecraft_2/data/ScannerAreaCapability/DefaultScannerArea.java
+++ src/main/java/net/cubic/dronecraft_2/dronecraft_2/data/ScannerAreaCapability/DefaultScannerArea.java
@@ -17,22 +17,6 @@ public class DefaultScannerArea implements IScannerArea {
     @Override
     public void AddScanner(BlockPos blockPos, int radius) {
         this.Scanners.add(new ScannerFormat(blockPos, radius)); // this now works because I made the default value a new arraylist
-        /*
-        List<ScannerFormat> temp;
-        ScannerFormat form = new ScannerFormat(blockPos, radius);
-        if (this.Scanners != null){
-            System.out.println("Not first time writing to list, there should already be something in it");
-            temp = this.Scanners;
-            temp.add(form);
-
-        }
-        else{
-            System.out.println("First time writing to scannerlist because list is null so it is set as not null with the data");
-            temp = new ArrayList<>();
-            temp.add(form);
-        }
-        this.SetScanners(temp);
-        */
     }
 
     @Override
