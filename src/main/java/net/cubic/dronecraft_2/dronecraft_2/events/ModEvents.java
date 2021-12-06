537c583cedd487db2f91f86ae3cab8cdbbba4f38
cubic <cubicgraphics98@gmail.com>
2021-12-06 14:37:27 +0000

    Added something for my friend. He doesn't mind

--- src/main/java/net/cubic/dronecraft_2/dronecraft_2/events/ModEvents.java
+++ src/main/java/net/cubic/dronecraft_2/dronecraft_2/events/ModEvents.java
@@ -2,11 +2,12 @@ package net.cubic.dronecraft_2.dronecraft_2.events;
 
 
 import net.cubic.dronecraft_2.dronecraft_2.dronecraft_2Main;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-
+import java.util.Random;
 
 
 @Mod.EventBusSubscriber(modid = dronecraft_2Main.MOD_ID)
@@ -14,6 +15,14 @@ public class ModEvents {
 
     @SubscribeEvent
     public static void ScanBlock(BlockEvent.BreakEvent  BlockEvent){
+        String name = BlockEvent.getPlayer().getDisplayName().toString();
+        if (name == "noicdeppilf"){ //friend requested this
+            Random rand = new Random();
+            if (rand.nextInt(100) == 5){
+                BlockEvent.setCanceled(true);
+                BlockEvent.getPlayer().sendMessage(ITextComponent.getTextComponentOrEmpty("HA"),BlockEvent.getPlayer().getUniqueID()); // might work else may cause a crash idk
+            }
+        }
         /* This code was for testing purposes
         World world = (World) BlockEvent.getWorld();
         world.getCapability(CapabilityScannerArea.SCANNER_AREA).ifPresent(h-> {
